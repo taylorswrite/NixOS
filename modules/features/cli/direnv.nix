c6711d32b8fdfc590417bee75bf091dfb05f25a1
William Martinez <wtmartinez@ucla.edu>
2026-01-31 18:20:43 -0800

    Added Direnv

---
+++ modules/features/cli/direnv.nix
@@ -0,0 +1,12 @@
+{ self, ... }:
+{
+  flake.nixosModules.direnv = { config, ... }: {
+    home-manager.users."${config.my.user}" = {
+      programs.direnv = {
+        enable = true;
+        nix-direnv.enable = true;
+        enableFishIntegration = true;
+      };
+    };
+  };
+}
--- modules/hosts/pain/pain.nix
+++ modules/hosts/pain/pain.nix
@@ -2,30 +2,34 @@
 {
   flake.nixosConfigurations.pain = inputs.nixpkgs.lib.nixosSystem {
     modules = [
-      ./_hardware-configuration.nix
-      self.nixosModules.common # Home Manager, Locale, Base Utils
-      self.nixosModules.grub # Grub & EFI
-      self.nixosModules.plymouth # Boot Animation (Silent)
-      self.nixosModules.nvidiaOpen # Nvidia Drivers & Graphics
-      self.nixosModules.wifiImpala # Lightweight IWD + Impala TUI
-      self.nixosModules.ssh # Hardened SSH
-      self.nixosModules.tailscale # VPN
-      self.nixosModules.fail2ban # Security
-      self.nixosModules.sddm # Login Manager
-      self.nixosModules.swayNvidia # Main WM (Sway + Nvidia Fixes)
-      self.nixosModules.xfce # Backup Desktop
-      self.nixosModules.mako # Notifications
-      self.nixosModules.fish # Shell Config (Aliases/Init)
-      self.nixosModules.starship # Prompt
-      self.nixosModules.kitty # Kitty
-      self.nixosModules.tmux # Terminal Multiplexer
-      self.nixosModules.pomodoro # CLI Timer
-      self.nixosModules.firefox # Privacy Browser + Vertical Tabs
-      self.nixosModules.nvim # LazyVim
-      self.nixosModules.git # Git + Delta + LazyGit
-      self.nixosModules.dev # Languages (Python, R, Rust) & Tools
-      self.nixosModules.docker # Container Engine
-      self.nixosModules.kvm # VM Engine (Virt-Manager)
+      ./_hardware-configuration.nix # Nix Generated - Host Specific
+      self.nixosModules.common      # Home Manager, Locale, Base Utils
+      self.nixosModules.grub        # Grub & EFI
+      self.nixosModules.plymouth    # Boot Animation (Silent)
+      self.nixosModules.nvidiaOpen  # Nvidia Drivers & Graphics
+      self.nixosModules.wifiImpala  # Lightweight IWD + Impala TUI
+      self.nixosModules.ssh         # Hardened SSH
+      self.nixosModules.tailscale   # VPN
+      self.nixosModules.fail2ban    # Security
+      self.nixosModules.sddm        # Login Manager
+      self.nixosModules.swayNvidia  # Main WM (Sway + Nvidia Fixes)
+      self.nixosModules.xfce        # Backup Desktop
+      self.nixosModules.mako        # Notifications
+      self.nixosModules.fish        # Shell Config (Aliases/Init)
+      self.nixosModules.starship    # Prompt
+      self.nixosModules.kitty       # Kitty
+      self.nixosModules.tmux        # Terminal Multiplexer
+      self.nixosModules.pomodoro    # CLI Timer
+      self.nixosModules.firefox     # Privacy Browser + Vertical Tabs
+      self.nixosModules.nvim        # LazyVim
+      self.nixosModules.git         # Git + Delta + LazyGit
+      self.nixosModules.dev         # Languages (Python, R, Rust) & Tools
+      self.nixosModules.docker      # Container Engine
+      self.nixosModules.kvm         # VM Engine (Virt-Manager)
+      self.nixosModules.direnv      # Auto-load dev environments (.envrc integration)
+      self.nixosModules.audio       # Pipewire (Sound) & Audio Group Permissions
+      self.nixosModules.video       # OpenGL (Graphics), Webcam & Brightness Permissions
+      self.nixosModules.bluetooth   # Bluetooth Hardware & GUI Manager (Blueman)
 
       # Machine-specific
       (
