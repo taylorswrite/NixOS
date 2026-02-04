7f8f53731a67cd3008abee4fda389fac7e75d278
William Martinez <wtmartinez@ucla.edu>
2026-02-04 13:23:33 -0800

    Finalized spotify

--- modules/features/applications/spotify.nix
+++ modules/features/applications/spotify.nix
@@ -1,18 +1,31 @@
-{ inputs, self, ... }:
-{
-  flake.nixosModules.spotify = { config, lib, pkgs, ... }: {
-    nixpkgs.config.allowUnfree = true;
+{ config, lib, pkgs, ... }: {
+  flake.nixosModules.spotify = { pkgs, config, ... }: {
+    services.spotifyd = {
+      enable = true;
+      settings = {
+        global = {
+          username_cmd = "cat ${config.sops.secrets.spotify_email.path}";
+          password_command = "cat ${config.sops.secrets.spotify_password.path}";
+          device_name = "pain";
+          bitrate = 320;
+          backend = "pulseaudio"; 
+          volume_normalization = true;
+          initial_volume = "90";
+        };
+      };
+    };
+
     environment.systemPackages = with pkgs; [
-      (symlinkJoin {
-        name = "spotify";
-        paths = [ spotify ];
-        buildInputs = [ makeWrapper ];
-        postBuild = ''
-          wrapProgram $out/bin/spotify \
-            --set NIXOS_OZONE_WL 1 \
-            --add-flags "--enable-features=UseOzonePlatform --ozone-platform=wayland"
-        '';
-      })
+      spotifyd
+      spotify-qt
     ];
+
+    home-manager.users."${config.my.user}" = {
+      xdg.configFile."spotify-qt/config.json".text = builtins.toJSON {
+        client = {
+          use_internal_player = false; 
+        };
+      };
+    };
   };
 }
