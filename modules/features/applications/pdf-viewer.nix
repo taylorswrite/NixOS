4bfeda55887c2f168bf65234d9679b710778f97a
William Martinez <wtmartinez@ucla.edu>
2026-02-04 13:29:58 -0800

    Fixed pdf viewer

--- modules/features/applications/pdf-viewer.nix
+++ modules/features/applications/pdf-viewer.nix
@@ -3,7 +3,7 @@
   # Flake part for Okular as the default
   flake.nixosModules.okular = { config, pkgs, ... }: {
     home-manager.users."${config.my.user}" = {
-      home.packages = [ pkgs.okular pkgs.zathura ];
+      home.packages = [ pkgs.kdePackages.okular pkgs.zathura ];
       xdg.mimeApps = {
         enable = true;
         defaultApplications."application/pdf" = [ "org.kde.okular.desktop" ];
@@ -14,7 +14,7 @@
   # Flake part for Zathura as the default
   flake.nixosModules.zathura = { config, pkgs, ... }: {
     home-manager.users."${config.my.user}" = {
-      home.packages = [ pkgs.okular pkgs.zathura ];
+      home.packages = [ pkgs.kdePackages.okular pkgs.zathura ];
       xdg.mimeApps = {
         enable = true;
         defaultApplications."application/pdf" = [ "org.pwmt.zathura.desktop" ];
