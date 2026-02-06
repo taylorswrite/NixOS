9a56dc5d008fe4b2d5983399503c505ea6dbd339
William Martinez <wtmartinez@ucla.edu>
2026-02-05 21:30:36 -0800

    Added gaming feature

--- modules/features/common/gaming.nix
+++ modules/features/common/gaming.nix
@@ -1,12 +1,39 @@
 { self, ... }:
 {
-  # 1. Base Gaming Module: For systems without a dedicated GPU (e.g., ThinkPad)
   flake.nixosModules.gaming = { config, pkgs, ... }: {
     programs.steam = {
       enable = true;
       remotePlay.openFirewall = true;
       dedicatedServer.openFirewall = true;
       localNetworkGameTransfers.openFirewall = true;
+      extraPackages = with pkgs; [
+        xorg.libXcursor
+        xorg.libXi
+        xorg.libXinerama
+        xorg.libXScrnSaver
+        libpng
+        libpulseaudio
+        libvorbis
+        stdenv.cc.cc.lib
+        libkrb5
+        keyutils
+      ];
+    };
+
+    # Essential for Steam, Heroic, and Lutris to handle 32-bit game binaries
+    hardware.graphics = {
+      enable = true;
+      enable32Bit = true;
+      extraPackages32 = with pkgs.pkgsi686Linux; [
+        libva
+        libvdpau
+        pipewire
+        libpulseaudio
+        libXtst
+        libXi
+        gtk2
+        gdk-pixbuf
+      ];
     };
 
     programs.gamemode.enable = true;
@@ -14,13 +41,9 @@
     environment.systemPackages = with pkgs; [
       mangohud
       protonup-qt
-      lutris
-      bottles
+      heroic    # For Epic, GOG, and Amazon
+      lutris    # For EA, Ubisoft, and community scripts
+      bottles   # Helpful for isolated Windows environments
     ];
-
-    hardware.graphics = {
-      enable = true;
-      enable32Bit = true;
-    };
   };
 }
