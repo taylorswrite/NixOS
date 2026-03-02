cdab32256db1511d8aed2152a4fbef9545fbd704
William Martinez <wtmartinez@ucla.edu>
2026-03-02 10:11:24 -0800

    Updated Kmonad to use hold tap release.

--- modules/features/hardware/keyboard/kmonadThinkpad.nix
+++ modules/features/hardware/keyboard/kmonadThinkpad.nix
@@ -32,19 +32,35 @@
                                                                       down
             )
 
+            
             (defalias
               esc_ctl esc
-              hm_a (tap-hold 150 a lalt)
-              hm_s (tap-hold 150 s lsft)
-              hm_d (tap-hold 150 d lmet)
-              hm_f (tap-hold 150 f lctl)
-              hm_j (tap-hold 150 j rctl)
-              hm_k (tap-hold 150 k rmet)
-              hm_l (tap-hold 150 l rsft)
-              hm_sc (tap-hold 150 ; ralt)
-              g_nav (tap-hold 150 g (layer-toggle nav_layer))
-              h_nums (tap-hold 150 h (layer-toggle h_num_layer))
+              hm_a (tap-hold-next-release 150 a lalt)
+              hm_s (tap-hold-next-release 150 s lsft)
+              hm_d (tap-hold-next-release 150 d lmet)
+              hm_f (tap-hold-next-release 150 f lctl)
+              hm_j (tap-hold-next-release 150 j rctl)
+              hm_k (tap-hold-next-release 150 k rmet)
+              hm_l (tap-hold-next-release 150 l rsft)
+              hm_sc (tap-hold-next-release 150 ; ralt)
+              g_nav (tap-hold-next-release 150 g (layer-toggle nav_layer))
+              h_nums (tap-hold-next-release 150 h (layer-toggle h_num_layer))
             )
+
+            ;; strict tap and hold
+            ;; (defalias
+            ;;   esc_ctl esc
+            ;;   hm_a (tap-hold 120 a lalt)
+            ;;   hm_s (tap-hold 120 s lsft)
+            ;;   hm_d (tap-hold 120 d lmet)
+            ;;   hm_f (tap-hold 120 f lctl)
+            ;;   hm_j (tap-hold 120 j rctl)
+            ;;   hm_k (tap-hold 120 k rmet)
+            ;;   hm_l (tap-hold 120 l rsft)
+            ;;   hm_sc (tap-hold 120 ; ralt)
+            ;;   g_nav (tap-hold 120 g (layer-toggle nav_layer))
+            ;;   h_nums (tap-hold 120 h (layer-toggle h_num_layer))
+            ;; )
 
             (deflayer main
               esc  f1   f2   f3   f4   f5   f6   f7   f8   f9   f10  f11  f12  power del
@@ -61,7 +77,7 @@
               _    _    _    _    _    _    _    _    _    _    _    _    _    _
               _    _    _    _    _    _    =    -    \(    \)    ]    _    _    _
               _    @hm_a @hm_s @hm_d @hm_f @g_nav left down up   rght @hm_k @hm_l @hm_sc
-              _    _    _    _    _    caps bspc del  grv  .    /    _
+              _    _    _    _    _    caps   -   -  grv  .    /    _
               _    _    _    _          _          _    _    _    _    _
                                                               _
             )
@@ -71,7 +87,7 @@
               _    _    _    _    _    _    _    _    _    _    _    _    _    _
               _    1    2    3    4    5    _    _    _    _    _    _    _    _
               _    6    7    8    9    0    @h_nums @hm_j @hm_k @hm_l @hm_sc ' _
-              _    _    _    _    _    _    _    _    _    _    _    _
+              _    _    _    \    [    ]    _    _    _    _    _    _
               _    _    _    _          _          _    _    _    _    _
                                                               _
             )
